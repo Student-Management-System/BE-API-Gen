70de8cf349da1d119af45e569b9a01c2a74c260c
Elscha <elscha@sse.uni-hildesheim.de>
2020-01-23 16:17:20 +0100

    Adds build JAR-with-Dependency target

--- generateAPI.sh
+++ generateAPI.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# Parameters
 generator=https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.16.jar
 dest=API
+jarName=StudentMgmt-Backend-API
 
 # Prerequisites
 wget $generator -O swagger-codegen-cli.jar
@@ -10,6 +13,10 @@ mkdir $dest
 java -jar swagger-codegen-cli.jar generate -i http://147.172.178.30:3000/api-json -l java -o $dest
 
 # Package
+cp -f pom.xml $dest
 cd $dest
-mvn clean compile source:jar-no-fork
-#assembly:single
+mvn clean compile source:jar-no-fork assembly:single
+
+# Rename results
+mv target/swagger-java-client-1.0.0-jar-with-dependencies.jar target/${jarName}.jar
+mv target/swagger-java-client-1.0.0-sources.jar target/${jarName}-src.jar

d5821f3a3771507dc942fa9c2ed8dfc8f144ddba
Elscha <elscha@sse.uni-hildesheim.de>
2020-01-23 15:31:31 +0100

    Initial version of generation script

---
+++ generateAPI.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+generator=https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.16.jar
+dest=API
+
+# Prerequisites
+wget $generator -O swagger-codegen-cli.jar
+mkdir $dest
+
+# Generate API
+java -jar swagger-codegen-cli-3.0.16.jar generate -i http://147.172.178.30:3000/api-json -l java -o $dest
+
+# Package
+cd $dest
+mvn clean compile assembly:single

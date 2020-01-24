e1660c7e95e7ca96b2f638adc1f94a1ce83ec943
Elscha <elscha@sse.uni-hildesheim.de>
2020-01-24 12:16:01 +0100

    Adds test if web service is ready

--- generateAPI.sh
+++ generateAPI.sh
@@ -5,23 +5,33 @@ generator=https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-c
 cli=swagger-codegen-cli.jar
 dest=API
 jarName=StudentMgmt-Backend-API
+apiURL=http://147.172.178.30:3000/api-json
 
 # Prerequisites
 if [ ! -f "$cli" ]; then
     # Download Swagger Code Generator
-    wget $generator -O $cli
+    wget "$generator" -O "$cli"
 fi
-rm -r $dest
-mkdir $dest
+rm -r "$dest"
+mkdir "$dest"
+
+# Wait until web service is up (see https://stackoverflow.com/a/12748070)
+code=0
+i=0
+while [ "$code" -ne 200 ] && [ $i -le 30 ]; do
+    sleep 1
+    code=$(curl -sL -w "%{http_code}\\n" "$apiURL" -o /dev/null)
+    i=$i+1
+done
 
 # Generate API
-java -jar $cli generate -i http://147.172.178.30:3000/api-json -l java -o $dest
+java -jar "$cli" generate -i "$apiURL" -l java -o "$dest"
 
 # Package
-cp -f pom.xml $dest
-cd $dest
+cp -f pom.xml "$dest"
+cd "$dest"
 mvn clean compile source:jar-no-fork assembly:single
 
 # Rename results
-mv target/swagger-java-client-1.0.0-jar-with-dependencies.jar target/${jarName}.jar
-mv target/swagger-java-client-1.0.0-sources.jar target/${jarName}-src.jar
+mv target/swagger-java-client-1.0.0-jar-with-dependencies.jar "target/${jarName}.jar"
+mv target/swagger-java-client-1.0.0-sources.jar "target/${jarName}-src.jar"
